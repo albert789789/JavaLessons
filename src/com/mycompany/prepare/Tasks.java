722b1fc2798e1af40b8ca04a6244926b0eed4962
Albert <zz.albert.ohanyan@gmail.com>
2022-06-11 15:56:22 +0400

    Commit 02

--- src/com/mycompany/prepare/Tasks.java
+++ src/com/mycompany/prepare/Tasks.java
@@ -6,9 +6,9 @@ public class Tasks {
 
     public static void main(String[] args) {
 
-        System.out.println("Test 02");
+        System.out.println("Test 01");
 
-        System.out.println("Test 03");
+        System.out.println("Test 02");
 
         System.out.println("Test 04");
     }
